1c78be26bab7adf7dc64fff5e46db785c6f1ea2b
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-08-21 18:17:56 -0400

    Moved from old repo

---
+++ js/about.js
@@ -0,0 +1,9 @@
+const nameColor = document.querySelector('h1');
+nameColor.addEventListener('mouseover', () => {
+    nameColor.style.color = '#8aa1b8';
+});
+
+var iconColor = document.querySelectorAll('i');
+iconColor.addEventListener('mouseover', () => {
+    iconColor.style.color = '#8aa1b8';
+});
---
+++ js/contact.js
@@ -0,0 +1,33 @@
+const nameColor = document.querySelector('h1');
+nameColor.addEventListener('mouseover', () => {
+    nameColor.style.color = '#8aa1b8';
+});
+
+function nameExample(event) {
+    document.getElementById("name").value = "FirstName LastName";
+}
+
+function emailExample(event) {
+    document.getElementById("email").value = "email@address.com";
+} 
+
+function nameClear (event) {
+    document.getElementById("name").value = "";
+}
+
+function emailClear (event) {
+    document.getElementById("email").value = "";
+}
+
+const formName = document.getElementById("name");
+formName.addEventListener('mouseover', nameExample);
+formName.addEventListener('click', nameClear);
+
+const formEmail = document.getElementById("email");
+formEmail.addEventListener('mouseover', emailExample);
+formEmail.addEventListener('click', emailClear);
+
+const formSubmit = document.getElementById("submit");
+formSubmit.addEventListener('click', () => {
+    alert('Thank you!');
+});
